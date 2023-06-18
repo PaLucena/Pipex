5cdf64e36b22ab7bc0a9a8f861fcb7d6a92933bf
palucena <palucena@student.42malaga.com>
2023-06-18 21:19:46 +0200

    Not finished

---
+++ hdr/pipex.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/14 16:15:19 by palucena          #+#    #+#             */
+/*   Updated: 2023/06/18 19:45:30 by palucena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <sys/wait.h>
+# include "libftprintf/header_files/libft.h"
+
+// las funciones van aquí
+
+#endif
---
+++ src/pipex.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/14 16:23:02 by palucena          #+#    #+#             */
+/*   Updated: 2023/06/18 17:42:50 by palucena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// 1. Analizar el input
+// 2. Ejecutar los comandos
+// 3. Eliminar archivos temporales
+
+#include "pipex.h"
+
+int	main(int ac, char **av)
+{
+	ft_init_pipex();
+	ft_check_args();
+	ft_parse_cmds();
+	ft_parse_args();
+	while (cmds)
+		ft_exec();
+	ft_cleanup();
+	return (0);
+}
