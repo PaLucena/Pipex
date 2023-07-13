b0c21065c033784a3fe9669630b6713737c9c7d1
palucena <palucena@student.42malaga.com>
2023-07-13 14:00:37 +0200

    Fancier lib

--- libft/ft_isprint.c
+++ libft/ft_isprint.c
@@ -6,10 +6,16 @@
 /*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 11:37:45 by palucena          #+#    #+#             */
-/*   Updated: 2023/04/21 15:20:00 by palucena         ###   ########.fr       */
+/*   Updated: 2023/07/12 17:53:42 by palucena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @brief Checks for any printable character including space.
+ * 
+ * @param c -> character to check
+ * @return int -> 1 if c is printable, 0 otherwise
+ */
 int	ft_isprint(int c)
 {
 	if (c >= 32 && c <= 126)
